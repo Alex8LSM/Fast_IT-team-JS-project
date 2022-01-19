2ca6c9356b9aaf4cc84ab607b47b686d2f4802bd
Oleksandr Khvaleba <alex8inbox@gmail.com>
2022-01-20 01:11:25 +0200

    3rd fix

--- src/js/our-team-modal.js
+++ src/js/our-team-modal.js
@@ -1,5 +1,3 @@
-import teamCard from '../templates/team-card.hbs';
-import our_team_data from '../json/our-team.json';
 function renderModal() {
 
   const refs = {
@@ -11,7 +9,6 @@ function renderModal() {
   };
 
   refs.show_Team.addEventListener('click', onModalOpen);
-  refs.show_Team.addEventListener('click', showTeam),
   refs.btnModalclose.addEventListener('click', onModalClose);
   refs.myModal.addEventListener('click', onClickBackdrop);
  
@@ -40,70 +37,6 @@ function renderModal() {
     if (isEscKey) {
       onModalClose();
     }
-  }
-
-  function createTeam(teamData) {
-    let cards = '';
-    for (const member of teamData) {
-      cards += `<li class="team-cards__card">
-                <img
-                  class="team-cards__image"
-                  srcset='./images/our_team/${member.big_image}.jpg 2x'
-                  src='./images/our_team/${member.small_image}.jpg'
-                  width="450"
-                  height="294"
-                  alt="${member.alt}"
-                  loading="lazy"
-                />
-                <div class="team-card">
-                  <h3 class="team-card__title">${member.name}</h3>
-                  <p class="team-card__profession">${member.role}</p>
-                  <ul class="list-box social-icons team-card__social-icons">
-                    <li class="list social-icons__list">
-                      <a class="link social-icons__icon" href="${member.instagram}">
-                        <svg class="team-card__social-icon" width="20px" height="20px">
-                          <use href="./images/sprite.svg#instagram"></use>
-                        </svg>
-                      </a>
-                    </li>
-
-                    <li class="list social-icons__list">
-                      <a class="link social-icons__icon" href="${member.twitter}">
-                        <svg class="team-card__social-icon" width="20px" height="20px">
-                          <use href="./images/sprite.svg#twitter"></use>
-                        </svg>
-                      </a>
-                    </li>
-
-                    <li class="list social-icons__list">
-                      <a class="link social-icons__icon" href="${member.facebook}">
-                        <svg class="team-card__social-icon" width="20px" height="20px">
-                          <use href="./images/sprite.svg#facebook"></use>
-                        </svg>
-                      </a>
-                    </li>
-
-                    <li class="list social-icons__list">
-                      <a class="link social-icons__icon" href="${member.linkedin}">
-                        <svg class="team-card__social-icon" width="20px" height="20px">
-                          <use href="./images/sprite.svg#linkedin"></use>
-                        </svg>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-    </li>`;
-    }
-    const markup = `<ul class="list-box team-cards">` + cards + `</ul>`;
-    return markup;
-  }
-
-  function showTeam() {
-
-    // const markup = teamCard(our_team_data);
-    const markup = createTeam(our_team_data);
-    console.log(markup);
-    refs.ourTeamModal.innerHTML = markup;
   }
 }
 export { renderModal };

9b763109a26d3ba9b909d636fa8b3fc6254eca85
Vladymyr <85882951+Syglob@users.noreply.github.com>
2022-01-21 19:34:29 +0200

    add  demo spiner /photo

--- src/js/fetchTrendyMovie.js
+++ src/js/fetchTrendyMovie.js
@@ -1,11 +1,11 @@
 import ApiMovie from '/js/apiMovie';
-import filmCardTpl from '../partials/templates/filmCard.hbs'
+import filmCardTpl from '../partials/templates/filmCard.hbs';
 const trendyMovie = new ApiMovie();
 
 const card = document.querySelector('.main-container');
 const logoEl = document.querySelector('.logo');
 logoEl.addEventListener('click', onLogoClick);
-     
+
 renderMainPage();
 
 function onLogoClick(e) {
@@ -15,15 +15,15 @@ function onLogoClick(e) {
 
 export function renderMainPage() {
   trendyMovie.page = 1;
-    trendyMovie.putGenresAndCutReleaseDateToYear()     
+  trendyMovie
+    .putGenresAndCutReleaseDateToYear()
     .then(renderFilmsCard)
     .catch(error => {
-      console.log(error)      
+      console.log(error);
     });
 }
 
-function renderFilmsCard(trendyMovies) {    
-    const markup = filmCardTpl(trendyMovies)
-    card.insertAdjacentHTML('beforeend', markup)   
+function renderFilmsCard(trendyMovies) {
+  const markup = filmCardTpl(trendyMovies);
+  card.insertAdjacentHTML('beforeend', markup);
 }
-

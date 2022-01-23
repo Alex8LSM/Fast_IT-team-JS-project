41fe9b7be50d11561fd191dd71e9b8f1131c5a6e
mariiamlyuzan <86401047+mariiamlyuzan@users.noreply.github.com>
2022-01-23 14:12:58 +0200

    fix api search

--- src/js/apiMoviesSearch.js
+++ src/js/apiMoviesSearch.js
@@ -3,7 +3,7 @@ const BASE_URL = 'https://api.themoviedb.org/3/';
 const axios = require('axios');
 
 async function fetchMoviesSearch(query, page){
-    const movies = await axios.get(`${BASE_URL}search/movie/?api_key=${API_KEY}&query=${query}&page=${page}`);
+    const movies = await axios.get(`${BASE_URL}search/movie?api_key=${API_KEY}&query=${query}&page=${page}`)
     return movies.data.results;
 }
 

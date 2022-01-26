1405d79ea1d3fef38b33cb761d4e79873160a25a
veriano <veriano@ukr.net>
2022-01-26 17:49:38 +0200

    update

--- src/js/myLibrary.js
+++ src/js/myLibrary.js
@@ -1,35 +1,28 @@
 const axios = require('axios');
+import { removePagination } from './paginationPages';
 import filmCardTpl from '../partials/templates/filmCard.hbs';
 
   const watchedBtn = document.querySelector('.header-watched');
   const queueBtn = document.querySelector('.header-queue');
   const filmContainer = document.querySelector('.main-container-films');
-
+  const emptyTitle = document.querySelector('.container-empty__title');
+  const emptyImage = document.querySelector('.container-empty__image');
   
   queueBtn.addEventListener('click', onQueueBtnClick);
   watchedBtn.addEventListener('click', onWatchedBtnClick);
-
-
-async function onQueueBtnClick() {
-  const STORAGE_KEY = 'queue';
-
-  const QueueIds = JSON.parse(localStorage.getItem( STORAGE_KEY ));
   
-  const moviesQueueList = [];
-  
-  if (QueueIds !== null) {
-    for (const id of QueueIds) {
-      moviesQueueList.push(await fetchMovie(id));
-    }
-  
-    markupMovies(moviesQueueList);
-  }
-}
-
 async function onWatchedBtnClick() {
+  watchedBtn.classList.add('active');
+  queueBtn.classList.remove('active');
+  
   const STORAGE_KEY = 'watched';
 
-  const WatchedIds = JSON.parse(localStorage.getItem( STORAGE_KEY ));
+  const WatchedIds = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (!WatchedIds) {
+    filmContainer.innerHTML = '';
+    emptyTitle.classList.add('visible');
+    emptyImage.classList.add('visible');
+  }
 
   const moviesWatchedList = [];
   
@@ -42,7 +35,32 @@ async function onWatchedBtnClick() {
   }
 }
 
+async function onQueueBtnClick() {
+  queueBtn.classList.add('active');
+  watchedBtn.classList.remove('active');
+
+  const STORAGE_KEY = 'queue';
+
+  const QueueIds = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (!QueueIds) {
+    filmContainer.innerHTML = '';
+    emptyTitle.classList.add('visible');
+    emptyImage.classList.add('visible');
+  }
+  
+  const moviesQueueList = [];
+  
+  if (QueueIds !== null) {
+    for (const id of QueueIds) {
+      moviesQueueList.push(await fetchMovie(id));
+    }
+  
+    markupMovies(moviesQueueList);
+  }
+}
+
 function markupMovies(movies) {
+  removePagination();
   filmContainer.innerHTML = '';
   const markup = filmCardTpl(movies);
   filmContainer.insertAdjacentHTML('beforeend', markup);
@@ -55,7 +73,6 @@ async function fetchMovie(id) {
 
   try {
     const response = await axios(`${BASE_URL}movie/${id}?api_key=${API_KEY}&language=en-US`);
-    console.log(response);
     return response.data;
   } catch (error) {
     console.log(error);
